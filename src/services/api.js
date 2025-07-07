a8ff8c1021c75a3c915a428011359fb8340d7402
Xrivera27 <griveram27@gmail.com>
2025-07-07 16:07:05 -0600

    Configurar API para producción

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // ✅ Verifica que sea la URL correcta
+  baseURL: process.env.VUE_APP_API_URL || 'https://sistemacotizacionbackend.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
