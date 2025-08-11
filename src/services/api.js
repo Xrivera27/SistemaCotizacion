56a83b8c458d7df2b29dc07e3a60058e844376d7
Xrivera27 <griveram27@gmail.com>
2025-08-11 16:08:01 -0600

    Se espere arreglar error de render

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+ baseURL: process.env.VUE_APP_API_URL || 'https://sistemacotizacionbackend.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
